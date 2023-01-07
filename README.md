4b0b59fc37f22f17412d2b3df6422c002c3fb516
Metehan Kan <mete@MacBook-Air-de-Metehan.local>
2023-01-07 23:21:57 +0100

    new version, finished game, random placements

--- README.md
+++ README.md
@@ -9,12 +9,22 @@
 
 - [X] Considère une bonne coordonnée comme bonne puis enchaine direct apres en disant que non, alors que la premiere coordonnée est bonne et est inscrite dans les grilles.
 
+
 - [X] Le systeme de coule ne marche pas : on ne voit pas de 'o' quand tout les points d'un navire sont touchés
+
 
 - [ ] Ajouter de jolies statistiques pour epater le joueur. Exemple: nombre de combo d'impacts, le temps que prend une partie.
 
+
 - [ ] Implementer un meilleur random (simplement aléatoire..)
+
 
 - [ ] Ameliorer l'intelligence artificielle. Exemple: utiliser les 'X' pour se deplacer a droite,gauche,bas,haut de celle ci.
 
-- [ ] Faire un beau menu pour choisir le mode de jeu. Exemple: choisir la difficuté de l'intelligence artificielle, le mode de jeu (belge ou classique, selon le nombre de bateau qu'on souhaite placer). Afficher le nom du jeu, les developpeurs.
+
+- [X] Faire un beau menu pour choisir le mode de jeu. Exemple: choisir la difficuté de l'intelligence artificielle, le mode de jeu (belge ou classique, selon le nombre de bateau qu'on souhaite placer). Afficher le nom du jeu, les developpeurs.
+
+
+- [ ] Interface a terminer: fair l'affichage de bateaux
+
+
--- battlecheap.c
+++ battlecheap.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <time.h>
 
 const int name_len = 32;
 
@@ -13,11 +15,6 @@ struct Ship
     int finito;
 };
 typedef struct Ship ship;
-
-// ship porteAvions = {.name = "Porte-avions", .length = 5};
-// ship croiseur = {.name = "Croiseur", .length = 4};
-// ship contreTorpilleur = {.name = "Contre-torpilleurs", .length = 3};
-// ship torpilleur = {.name = "Torpilleur", .length = 2};
 
 ship crewPlayer[5] = {
     {.name = "Porte-avions", .length = 5, .isPlaced = 0},
@@ -38,54 +35,170 @@ const int offset_size = 16;
 const char alphabet[10] = "ABCDEFGHIJ";
 const int lines_len = (len + 1) * 4;
 
-char *
-makeGrid();                                                              // créer un tableau
-void offset();                                                           // affiche un espace d elongeur off slent
-void printl(int n);                                                      // affiche une ligne de longueue 44
-void showGrid(char tab[]);                                               // fonction de debogage
-void showTable(char tab_j[], char tab_b[]);                              // affiche la grille
-int ALPHANUM(char c);                                                    // transforme une lettre en entier
-char BOOLCHAR(int bool);                                                 // Renvoie 'Y' si bool == 1, 'N' sinon
-void showShips(ship list[]);                                             // Affiche les different navires
-void initGridPlayer(char tab[], ship list[]);                            // demande les coordonées des navires
-void initGridBot(char tab[], ship list[]);                               // initialise aleatoirement les navires du bot
-int gridAuth(char tab[], int p, char x, int y, char dir, ship list[]);   // vereifie si les coordonées int du sens
-void gridPlace(char tab[], int p, char x, int y, char dir, ship list[]); // place le navire selon les coordonées
-void torpedo(char grid_ATTACK_1[], char grid_2[], char x, int y);        // attaque du joueur
-void requestTorpedo(char grid_ATTACK_1[], char grid_2[]);                // pour rendre le code souple
-void torpedoCalibration(char grid_ATTACK_1[], char grid_2[]);            // pour le bot
-int fatalTorpedo(char grid_ATTACK_1[], char grid_2[], ship list[]);      // verifie que le joueur 1 a fait coulé un navire du joueur 2
-int peaceQuestionMark(ship list_1[], ship list_2[]);
+void bsh();                                                                 // battlecheap shell
+void rules();                                                               // les regles
+void play();                                                                // le 'jeu'
+char *makeGrid();                                                           // créer un tableau
+void offset();                                                              // affiche un espace d elongeur off slent
+void printl(int n);                                                         // affiche une ligne de longueue 44
+void showGrid(char tab[]);                                                  // fonction de debogage
+void showTable(char tab_j[], char tab_b[]);                                 // affiche la grille
+int ALPHANUM(char c);                                                       // transforme une lettre en entier
+char BOOLCHAR(int bool);                                                    // Renvoie 'Y' si bool == 1, 'N' sinon
+void showShips(ship list[]);                                                // Affiche les different navires
+void initGridPlayer(char tab[], ship list[]);                               // demande les coordonées des navires
+void initGridBot(char tab[], ship list[]);                                  // initialise aleatoirement les navires du bot
+int gridAuth(char tab[], int p, char x, int y, char dir, ship list[]);      // vereifie si les coordonées int du sens
+void gridPlace(char tab[], int p, char x, int y, char dir, ship list[]);    // place le navire selon les coordonées
+void torpedo(char grid_ATTACK_1[], char grid_2[], char x, int y, int *hit); // attaque du joueur
+void requestTorpedo(char grid_ATTACK_1[], char grid_2[], int *hit);         // pour rendre le code souple
+void torpedoCalibration(char grid_ATTACK_1[], char grid_2[], int *hit);     // pour le bot
+int fatalTorpedo(char grid_ATTACK_1[], char grid_2[], ship list[]);         // verifie que le joueur 1 a fait coulé un navire du joueur 2
+int peaceQuestionMark(ship list_1[], ship list_2[], int *a, int *b);
 void processing();
 
 int main()
 {
+    srand(time(0));
+    bsh();
+    return 0;
+}
+
+void bsh()
+{
+    char str[] = "\n _           _   _   _           _                      \n| |         | | | | | |         | |                     \n| |__   __ _| |_| |_| | ___  ___| |__   ___  __ _ _ __  \n| '_ \\ / _` | __| __| |/ _ \\/ __| '_ \\ / _ \\/ _` | '_ \\ \n| |_) | (_| | |_| |_| |  __/ (__| | | |  __/ (_| | |_) |\n|_.__/ \\__,_|\\__|\\__|_|\\___|\\___|_| |_|\\___|\\__,_| .__/ \n                                                 | |    \n                                                 |_|\n\n";
+    for (int i = 0; i < sizeof(str) / sizeof(char); i += 1)
+    {
+        printf("%c", str[i]);
+    }
+    // Saluer le joeur
+    printf("\nbsh>Voici les differentes commandes disponibles : \n");
+    printf("     - \033[1mrules\033[0m: ennonce les regles\n");
+    printf("     - \033[1mships\033[0m: Fait une liste des bateaux\n");
+    printf("     - \033[1mhelp\033[0m: Fait une liste des bateaux\n");
+    printf("     - \033[1mplay\033[0m: commence la partie\n");
+    printf("     - \033[1mexit\033[0m: arrete la partie\n");
+    char tmp[10] = {' '};
+    int finito = 0;
+    while (!finito)
+    {
+        printf("bsh>");
+        if (scanf("%s", tmp) == 1)
+        {
+            if (strcmp(tmp, "rules") == 0)
+            {
+                rules();
+            }
+            else if (strcmp(tmp, "ships") == 0)
+            {
+                // Faire la liste des bateaux
+                // listShips(crewBot);
+            }
+            else if (strcmp(tmp, "help") == 0)
+            {
+                printf("Voici les differents controles\n");
+                printf("  - Quand c'est votre tour, il suffit, pour attaquer, d'entrer une coordonnée.\n    Format: [A:J],[1:10]. Ne pas oublier la virgule.\n    Exemple: bsh>B,5\n");
+            }
+            else if (strcmp(tmp, "play") == 0)
+            {
+                // Renvoyer vers la fonction jouer
+                play();
+            }
+            else if (strcmp(tmp, "exit") == 0)
+            {
+                // arrete la partie
+                finito = 1;
+            }
+            else
+            {
+                printf("  Command %s wasn't found", tmp);
+                fflush(stdin);
+            }
+        }
+    }
+}
+
+void rules()
+{
+    printf("\033[1mRegles\033[0m\n");
+    printf("Vous allez jouer contre une intelligence artificielle à la \033[1mbattaille navale\033[0m. Chacun de vous aura deux grilles de 10x10 indicées de [A,J] et [1,10].\n");
+    printf("La première est l'emplacement de vos navires, que vous placerez.\n");
+    printf("La première est l'emplacement de vos navires, que vous placerez.\n");
+    printf("Chaque joueur à son tour annonce une case: soit \"touché\" pour un navire impacté mais encore en flotte, soit \"coulé\" si vous venez de couler un navier. Sinon \"plouf\"\n");
+    printf("--- --- --- --- --- --- --- --- --- ---\n");
+}
+
+void play()
+{
+    // Créer les tableaux
     char *grid_player = makeGrid();
     char *grid_ATTACK_player = makeGrid();
     char *grid_bot = makeGrid();
     char *grid_ATTACK_bot = makeGrid();
-    // initGridPlayer(grid_player, crewPlayer);
+
+    // initialiser les tableaux
     initGridBot(grid_player, crewPlayer);
     initGridBot(grid_bot, crewBot);
-    int i = 0;
-    while (!peaceQuestionMark(crewPlayer, crewBot))
-    {
 
-        showTable(grid_player, grid_ATTACK_player);
-        showGrid(grid_bot);
-        printf("Tour du joueur: \n");
-        requestTorpedo(grid_ATTACK_player, grid_bot);
-        if (
-            fatalTorpedo(grid_ATTACK_player, grid_bot, crewBot)) // ajouter quel navire a coulé
-            printf("Vous avez coulé un navire !\n");
-        printf("Tour du bot: \n");
-        processing();
-        torpedoCalibration(grid_ATTACK_bot, grid_player);
-        if (fatalTorpedo(grid_ATTACK_bot, grid_player, crewPlayer))
-            printf("Un de vos navires vient d'être détruit..\n");
+    // On continue de jouer tant que tout les navires d'au moins un joueur ne soient pas coulés
+    // A chaque debut de tour, on affiche les tableaux
+    // Si le joueur touche, il peut rejouer, sinon c'est au tour du bot, et ainsi de suite
+    int a, b;
+    while (!(peaceQuestionMark(crewPlayer, crewBot, &a, &b)))
+    {
+        printf("\033[1mC'est au tour du joueur\033[0m\n");
+        int combo = 1;
+        while (combo)
+        {
+            showTable(grid_player, grid_ATTACK_player);
+            int tmp = 0;
+            requestTorpedo(grid_ATTACK_player, grid_bot, &tmp);
+            if (fatalTorpedo(grid_ATTACK_player, grid_bot, crewBot))
+            {
+                printf("Coulé\n");
+            }
+            if (peaceQuestionMark(crewPlayer, crewBot, &a, &b))
+            {
+                break;
+            }
+            if (tmp == 0)
+            {
+                combo = 0;
+            }
+        }
+        printf("\033[1mC'est au tour du bot\033[0m\n");
+        combo = 1;
+        while (combo)
+        {
+            int tmp = 0;
+            torpedoCalibration(grid_ATTACK_bot, grid_player, &tmp);
+            if (fatalTorpedo(grid_ATTACK_bot, grid_player, crewPlayer))
+            {
+                printf("Coulé\n");
+            }
+            if (peaceQuestionMark(crewPlayer, crewBot, &a, &b))
+            {
+                break;
+            }
+            {
+                if (tmp == 0)
+                {
+                    combo = 0;
+                }
+            }
+        }
     }
-    printf("Félicitations capitaine, vous avez coulé tous les bateaux !\nRetour à la base.");
-    return 0;
+    // La partie est finie
+    // On veut connaitre le gagnant
+    peaceQuestionMark(crewPlayer, crewBot, &a, &b);
+    if (a == 0)
+    {
+        printf("\033[1mC'est vous, le joueur, le GRAND gagnant de cette bataille navale !\033[0m\n");
+    }
+    else if (b == 0)
+    {
+        printf("\033[1mC'est vous, le joueur, le GRAND perdant de cette bataille navale ..\033[0m\n");
+    }
 }
 
 char *makeGrid()
@@ -167,13 +280,13 @@ void showTable(char tab_j[], char tab_b[])
         printf("%d  |", i);
         for (int j = 0; j < len; j += 1)
         {
-            printf(" %c |", tab_j[(i - 1) * 10 + j]); // char à l'emplacement de la ligne et colonne resp
+            printf(" \033[1;34m%c\033[0m |", tab_j[(i - 1) * 10 + j]); // char à l'emplacement de la ligne et colonne resp
         }
         offset();
         printf("%d  |", i);
         for (int j = 0; j < len; j += 1)
         {
-            printf(" %c |", tab_b[(i - 1) * 10 + j]); // char à l'emplacement de la ligne et colonne resp
+            printf(" \033[1;31m%c\033[0m |", tab_b[(i - 1) * 10 + j]); // char à l'emplacement de la ligne et colonne resp
         }
         printf("\n");
         printl(lines_len);
@@ -184,13 +297,13 @@ void showTable(char tab_j[], char tab_b[])
     printf("%d |", len);
     for (int i = 0; i < len; i += 1)
     {
-        printf(" %c |", tab_j[(len - 1) * 10 + i]);
+        printf(" \033[1;34m%c\033[1;0m |", tab_j[(len - 1) * 10 + i]);
     }
     offset();
     printf("%d |", len);
     for (int i = 0; i < len; i += 1)
     {
-        printf(" %c |", tab_b[(len - 1) * 10 + i]);
+        printf(" \033[1;31m%c\033[0m |", tab_b[(len - 1) * 10 + i]);
     }
     printf("\n");
     printl(lines_len);
@@ -371,7 +484,7 @@ void gridPlace(char tab[], int p, char x, int y, char dir, ship list[])
     }
 }
 
-void requestTorpedo(char grid_ATTACK_1[], char grid_2[])
+void requestTorpedo(char grid_ATTACK_1[], char grid_2[], int *hit)
 {
     printf("Entrez une coordonée :");
     char x;
@@ -381,64 +494,42 @@ void requestTorpedo(char grid_ATTACK_1[], char grid_2[])
     {
         fflush(stdin);
         printf("Mauvaise coordonnée chef.\n");
-        requestTorpedo(grid_ATTACK_1, grid_2);
+        requestTorpedo(grid_ATTACK_1, grid_2, hit);
     }
     else if (ALPHANUM(x) < 1 || ALPHANUM(x) > 10)
     {
         printf("Mauvaise coordonnée chef...\n");
-        requestTorpedo(grid_ATTACK_1, grid_2);
+        requestTorpedo(grid_ATTACK_1, grid_2, hit);
     }
     else if (!(y >= 1 && y <= 10))
     {
         printf("Mauvaise coordonnée chef..\n");
-        requestTorpedo(grid_ATTACK_1, grid_2);
+        requestTorpedo(grid_ATTACK_1, grid_2, hit);
     }
     else
     {
-        torpedo(grid_ATTACK_1, grid_2, x, y);
+        torpedo(grid_ATTACK_1, grid_2, x, y, hit);
     }
 }
 
-void torpedo(char grid_ATTACK_1[], char grid_2[], char x, int y)
+void torpedo(char grid_ATTACK_1[], char grid_2[], char x, int y, int *hit)
 {
-    // printf("Entrez une coordonée :");
-    // char x;
-    // int y;
-    // fflush(stdin);
-    // if (scanf("(%c,%d)", &x, &y) != 2)
-    // {
-    //     printf("Mauvaise coordonnée chef..\n");
-    //     torpedo(grid_ATTACK_1, grid_2);
-    // }
-    // else
-    // {
-
-    // x appartient a [ABCDEFGHIJ]
-    // 1 ≤ y ≤ 10
-    // if (ALPHANUM(x) < 1 || ALPHANUM(x) > 10)
-    // {
-    //     printf("Mauvaise coordonnée chef...\n");
-    //     requestTorpedo(grid_ATTACK_1, grid_2);
-    // }
-    // else if (!(y >= 1 && y <= 10))
-    // {
-    //     printf("Mauvaise coordonnée chef..\n");
-    //     requestTorpedo(grid_ATTACK_1, grid_2);
-    // }
     if (grid_2[ALPHANUM(x) - 1 + (y - 1) * 10] == '!' || grid_2[ALPHANUM(x) - 1 + (y - 1) * 10] == '-')
     {
         printf("Touché !\n");
         grid_ATTACK_1[ALPHANUM(x) - 1 + (y - 1) * 10] = 'X'; // pour dire qu'il y a un impact
         grid_2[ALPHANUM(x) - 1 + (y - 1) * 10] = 'X';        // pour dire qu'il y a une impact
+        *hit = 1;
     }
     else if (grid_2[ALPHANUM(x) - 1 + (y - 1) * 10] == ' ')
     {
         printf("Plouf, pas touché..\n");
         grid_ATTACK_1[ALPHANUM(x) - 1 + (y - 1) * 10] = '.';
+        *hit = 0;
     }
 }
 
-void torpedoCalibration(char grid_ATTACK_1[], char grid_2[])
+void torpedoCalibration(char grid_ATTACK_1[], char grid_2[], int *hit)
 {
     int isAvailable = 0;
     int x, y;
@@ -446,9 +537,9 @@ void torpedoCalibration(char grid_ATTACK_1[], char grid_2[])
     {
         x = rand() % 10;
         y = rand() % 10;
-        if (!(grid_ATTACK_1[x + y * 10] == 'X' || grid_ATTACK_1[x + y * 10] == 'o' || grid_ATTACK_1[x + y * 10] == '.'))
+        if (grid_ATTACK_1[x + y * 10] != 'X' || grid_ATTACK_1[x + y * 10] != 'o' || grid_ATTACK_1[x + y * 10] != '.')
         {
-            torpedo(grid_ATTACK_1, grid_2, x, y);
+            torpedo(grid_ATTACK_1, grid_2, x, y, hit);
             isAvailable = 1;
         }
     }
@@ -505,7 +596,7 @@ int fatalTorpedo(char grid_ATTACK_1[], char grid_2[], ship list[]) // TODO:ajout
     return 0;
 }
 
-int peaceQuestionMark(ship list_1[], ship list_2[])
+int peaceQuestionMark(ship list_1[], ship list_2[], int *player, int *bot)
 {
     int a, b = 1;
     for (int i = 0; i < 5; i += 1)
@@ -519,7 +610,8 @@ int peaceQuestionMark(ship list_1[], ship list_2[])
             b = 0;
         }
     }
-    printf("%d\n", (a + b) / 2);
+    *player = a;
+    *bot = b;
     return a + b;
 }
 
